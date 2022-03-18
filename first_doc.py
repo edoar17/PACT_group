fc0da4e4f33beb218ba72bd01ac817aa11d822ad
edoar17 <edoarvega@gmail.com>
2022-03-18 09:43:05 -0600

    Added monthly plots and sustainable global etf

--- first_doc.py
+++ first_doc.py
@@ -22,11 +22,13 @@ import statsmodels.api as sm
 def log_return(series):
     return np.log(series).diff()
 
-ticker = 'PGH.AX'
-
-PGH_ = yf.Ticker(ticker)
-pgh = PGH_.history(period='3y', interva='1d')
-
+def to_monthly(df, last_day=False):
+    if last_day==False:
+        out = df.groupby([df.index.year, df.index.month]).head(1)
+    else:
+        out = df.groupby([df.index.year, df.index.month]).tail(1)
+    return out
+ 
 def calcs(df):
     df['log_return'] = log_return(df['Close'])
     df['cum_ret'] = np.exp(df['log_return'].cumsum())
@@ -52,18 +54,24 @@ def graph1(df, title):
     axs[1].set_title('Cumulative Return')
     
     path = f"plots/{title}.png"
-    if not os.path.exists(path):
-        fig.savefig(f"plots/{title}.png", format='png')
+    # if not os.path.exists(path):
+    fig.savefig(f"plots/{title}.png", format='png')
     fig.show()
 
+   
 
+# Download PGH data
+ticker = 'PGH.AX'
+
+PGH_ = yf.Ticker(ticker)
+pgh = PGH_.history(period='5y', interva='1d')
 pgh = calcs(pgh)
 graph1(pgh, 'Price of PGH.AX')
 
 # From pandemic low
-pandemic_bottom = '2020-03-20'
+pandemic_bottom = '1900-01-01'
 pgh1 = calcs(pgh.loc[pgh.index>=pandemic_bottom])
-graph1(pgh1, 'Price of PGH.AX from pandemic low')
+# graph1(pgh1, 'Price of PGH.AX from pandemic low')
 
 # Check relevant ETFs
 indexes = {'EWA': 'iShares MSCI-Australia ETF',
@@ -71,7 +79,9 @@ indexes = {'EWA': 'iShares MSCI-Australia ETF',
            'FAIR.AX': 'Betashares Australian Sustainability Leaders ETF ',
            'MVS.AX': 'VanEck Vectors Small Companies Masters ETF',
            'DBC': 'Invesco DB Commodity Index Tracking Fund',
-           'DBO': 'Invesco DB Oil Fund'}
+           'DBO': 'Invesco DB Oil Fund',
+           #Sustainable ETFs
+           'ERTH':'Invesco MSCI Sustainable Future ETF '}
 
 currencies = {'AUDUSD=X': 'AUD/USD',
               'AUDJPY=X': 'AUD/JPY'}
@@ -85,11 +95,14 @@ competitors = {'PPG.AX': 'Pro-Pac Packaging Limited', #101M
 #Linear regressions
 def prep_features(df, df2):
     # df = df.loc[df.index>=pandemic_bottom]
-
-    data = df[['log_return']].join(df2['log_return'], rsuffix='_pact')
+    data = df[['log_return']].join(df2['log_return'], rsuffix='_pact') 
     data = data.dropna()
+    # data = to_monthly(data, last_day=(False))
+    # print(data)
     data['log_return'] = data['log_return']/data['log_return'].max()
     data['log_return_pact'] = data['log_return_pact']/data['log_return_pact'].max()
+    
+    # print(data)
     return data
 
 # plot returns
@@ -139,7 +152,7 @@ cum_ret = pgh1[['cum_ret']]
 
 for i in list(indexes):
     bench = yf.Ticker(i)
-    bench = bench.history(period='3y', interval='1d')
+    bench = bench.history(period='5y', interval='1d')
     #Calculate returns, cum_ret
     bench = calcs(bench.loc[bench.index>=pandemic_bottom])
     
@@ -163,7 +176,7 @@ for i in list(indexes):
 
 for i in list(competitors):
     bench = yf.Ticker(i)
-    bench = bench.history(period='3y', interval='1d')
+    bench = bench.history(period='5y', interval='1d')
     #Calculate returns, cum_ret
     bench = calcs(bench.loc[bench.index>=pandemic_bottom])
     
@@ -186,10 +199,10 @@ for i in list(competitors):
         
 for i in list(currencies):
     bench = yf.Ticker(i)
-    bench = bench.history(period='3y', interval='1d')
+    bench = bench.history(period='5y', interval='1d')
     #Calculate returns, cum_ret
     bench = calcs(bench.loc[bench.index>=pandemic_bottom])
-    
+    # print(bench)
     #Save data to two dfs
     col_name = i+'_log_return'
     col_name1 = i+'_cum_ret'
@@ -226,23 +239,28 @@ path = "plots/cum_returns.png"
 fig.savefig(path, format='png')
 fig.show()
 
-#Regression of pgh,asx200
-pgh = prices['log_return']
-asx = prices['^AXJO_log_return']
 
-lm = sm.OLS(pgh, asx)
-res = lm.fit()
-pred = res.predict(asx)
 
-with open('summary.txt', 'w') as fh:
-    fh.write(res.summary().as_text())
 
-print(res.summary())
+
+
+
+
+
+
+
+
+
+
+
+# #Regression of pgh,asx200
+# pgh = prices['log_return']
+# asx = prices['^AXJO_log_return']
+
+# lm = sm.OLS(pgh, asx)
+# res = lm.fit()
+# pred = res.predict(asx)
     
-reg = LinearRegression().fit(asx.values.reshape(-1,1), pgh.values.reshape(-1,1))
-pred = reg.predict(asx.values.reshape(-1,1))    
-score = reg.score(asx.values.reshape(-1,1), pgh.values.reshape(-1,1))
-reg.coef_
 
 
 
